f932db6e0751567c64544b1bf25a340317d69fb9
Dahis30 <mohamednadir637@gmail.com>
2024-02-11 16:54:35 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+
+    <form>
+      <label for="">Enter a title</label>
+      <input type="text" v-model="input" >
+    </form>
+    <br>
+    <button @click="generateSlug()">Generate a slug</button>
+    <br>
+    <br>
+    <div>
+      https://www.generateslug.com/ <span>{{ output }}</span>
+    </div>
+
+  </div>
+ 
+</template>
+
+<script>
+export default {
+
+  data:()=>({
+    input:'',
+    output:'',
+  }),
+  methods:{
+    generateSlug(){
+
+      // 1) convert the title to lowercasa
+      var Slug = this.input.toLowerCase() ;
+
+      // 2) replace the space around words with  dashes 
+      var Slug = Slug.replace(/\s+/g,'-') ;
+
+      // 3) replace ampersand with dashes and space
+      var Slug = Slug.replace(/&/,'-and-')
+
+
+      this.output= Slug;
+
+    },
+  }
+}
+</script>
